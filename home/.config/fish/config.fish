3f38cf0092da4c9d063cff50e881affae6969b37
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-10-10 16:15:55 +0200

    Added config.fish

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,15 @@
+begin
+    # Try with the users install
+    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+
+    # If user did not have his own, try the system one
+    if test "! -e $AUTOJUMP_PATH"
+        set AUTOJUMP_PATH /usr/share/autojump/autojump.fish
+    end
+
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    else
+        echo "autojump was not found on system, won't be available"
+    end
+end
