29a282f70bab66c72091ab991368e06e4706c7f0
Erik Bjäreholt <erik@bjareho.lt>
2017-09-01 10:20:11 +0200

    Created basic startup routine for ipython profile

---
+++ home/.ipython/profile_default/startup/00-imports.py
@@ -0,0 +1,8 @@
+from itertools import combinations, permutations
+from math import pi
+
+from time import sleep
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
