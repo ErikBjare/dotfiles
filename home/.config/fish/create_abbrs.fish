b1e290985c2fea406f63d71d5ba11146aeec64c1
Erik Bjäreholt <erik@bjareho.lt>
2018-01-14 00:28:40 +0100

    improvements

--- home/.config/fish/create_abbrs.fish
+++ home/.config/fish/create_abbrs.fish
@@ -1,6 +1,17 @@
 #!/usr/bin/env fish
 
+abbr --add eg "egrep"
+
+abbr --add archive-targz "tar xzvf"
+
 abbr --add gs "git status"
+abbr --add gss "git submodule summary"
 abbr --add gl "git log --all --graph --decorate --pretty"
 abbr --add gpl "git pull"
 abbr --add gps "git push"
+
+abbr --add pe "pipenv"
+
+abbr --add tmain "tmux attach -t main; or tmux new-session -t main"
+abbr --add tsys "tmux attach -t system; or tmux new-session -t system"
+
