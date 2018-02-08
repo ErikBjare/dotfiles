e2f372c4bd9ba7458ce2eb3aef3162b68af16da1
Erik Bjäreholt <erik@bjareho.lt>
2018-02-08 09:24:07 +0100

    a bunch of old changes that I've forgotten to commit

--- home/.xrandr/erb-main2.sh
+++ home/.xrandr/erb-main2.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+#set -e
+
 xrandr --output DP-1 --auto --rotate right
-xrandr --output HDMI-0 --auto --primary --mode 3840x2160 --refresh 60 --right-of DP-1
-xrandr --output DP-4 --auto --right-of HDMI-0
+xrandr --output HDMI-0 --auto --primary --mode 3840x2160 --right-of DP-1
+xrandr --output DP-2 --auto --right-of HDMI-0
 
 # Disabled displays
 xrandr --output DVI-D-0 --off
