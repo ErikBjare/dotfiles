0854a2187111d5a9016d46f5f1970b777762e6f1
Erik Bjäreholt <erik@bjareho.lt>
2018-02-08 09:26:44 +0100

    improved ipython3 default imports

--- home/.ipython/profile_default/startup/00-imports.py
+++ home/.ipython/profile_default/startup/00-imports.py
@@ -1,8 +1,22 @@
-from itertools import combinations, permutations
+# Standard library
+import re
+import itertools
 from math import pi
-
 from time import sleep
+from pprint import pprint
+from typing import Optional, List, Dict, Union, Any
+from importlib import reload
+from datetime import datetime, timezone
+from collections import defaultdict
 
+# PyPI packages
+import requests
+
+# Set up logging
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+
+# Shorthands
+req = requests
+it = itertools
