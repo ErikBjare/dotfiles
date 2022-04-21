7d8bde10d53429f7407cdc225ad7231fe8a6642c
Erik Bjäreholt <erik@bjareho.lt>
2022-04-21 13:11:18 +0200

    fix: fixed screenshot script (now puts it correctly in clipboard)

--- home/.bin/screenshot.sh
+++ home/.bin/screenshot.sh
@@ -21,13 +21,15 @@ if [ "$1" == "--help" ]; then
     exit 0
 elif [ "$1" == "--region" ]; then
     echo "Doing a region shot!"
-    escrotum --select $FILE || echo 'escrotum failed' && exit 1
+    escrotum --select $FILE || (echo 'escrotum failed' && exit 1)
     msg "Captured region"
 elif [ "$1" == "--fullscreen" ]; then
-    escrotum $FILE || echo 'escrotum failed' && exit 1
+    escrotum $FILE || (echo 'escrotum failed' && exit 1)
     msg "Captured fullscreen"
 else
     echo "Needs a command line argument, see --help for info."
     exit 1
 fi
+
+echo 'Putting in clipboard'
 xclip -sel clip -t image/png $FILE
