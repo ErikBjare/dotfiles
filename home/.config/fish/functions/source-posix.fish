5828096fc51854ec4d4a5feee018a74d347f5461
Erik Bjäreholt <erik@bjareho.lt>
2022-11-15 12:18:39 +0100

    fix: improved source-posix fish script

--- home/.config/fish/functions/source-posix.fish
+++ home/.config/fish/functions/source-posix.fish
@@ -1,5 +1,10 @@
+# Sources a sh/bash-compatible .env file into fish
 function source-posix
 	for i in (cat $argv)
+        # if line is a comment or empty, skip it
+        if test (echo $i | cut -c1) = "#" -o -z $i
+            continue
+        end
 		set arr (echo $i |tr = \n)
   		set -gx $arr[1] $arr[2]
 	end
