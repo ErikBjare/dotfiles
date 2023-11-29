33cfdfab1267e1a129d048455a6a62741605f688
Erik Bjäreholt <erik@bjareho.lt>
2023-11-29 12:04:31 +0100

    fix: more improvements

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -30,10 +30,13 @@ begin
     path_prepend ~/.gem/ruby/2.5.0/bin
     path_prepend ~/.gem/ruby/2.6.0/bin
     path_prepend ~/.gem/ruby/2.7.0/bin
-    path_prepend ~/.local/share/gem/ruby/3.0.0/bin
     # path_prepend /opt/homebrew/opt/ruby/bin
     # path_prepend /opt/homebrew/opt/ruby@2.7/bin
     path_prepend /opt/homebrew/opt/ruby@3/bin
+
+    # gems
+    path_prepend ~/.local/share/gem/ruby/3.0.0/bin       # linux
+    path_prepend /opt/homebrew/lib/ruby/gems/3.2.0/bin/  # macos
 
     # Setting brew JDK on macOS
     path_prepend /opt/homebrew/opt/openjdk/bin
