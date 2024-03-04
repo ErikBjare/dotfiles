811288acd725f1745f62d0b4c81bb6432218de28
Erik Bjäreholt <erik@bjareho.lt>
2024-03-04 18:20:31 +0100

    fix: added lua/plugins.lua

---
+++ home/.config/nvim/lua/plugins.lua
@@ -0,0 +1,21 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use({
+    "jackMort/ChatGPT.nvim",
+      config = function()
+        require("chatgpt").setup()
+      end,
+      requires = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim"
+      }
+  })
+end)
