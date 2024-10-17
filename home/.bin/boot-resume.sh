80af84ed36753c05008e1aff6eb52de2ae338a19
Erik Bjäreholt <erik@bjareho.lt>
2024-10-17 12:29:18 +0200

    added boot-resume.sh script

---
+++ home/.bin/boot-resume.sh
@@ -0,0 +1,23 @@
+# start apps commonly started on boot
+
+
+function start() {
+    # starts an app in the background (disowning), if it's not already running
+    cmd=$1
+    if ! pgrep -f $cmd; then
+        $cmd &
+    fi
+    sleep 0.5
+    # disown the process
+    if pgrep -f $cmd; then
+        disown
+    fi
+}
+
+
+start alacritty
+start firefox
+start standard-notes
+start roam-research
+start webcord
+start pavucontrol
