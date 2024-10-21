306d6950874b655892036ea321efb080f4d8123d
Erik Bjäreholt <erik@bjareho.lt>
2024-10-21 16:51:06 +0200

    misc stuff

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,6 +1,6 @@
 -- Neovim-specific Lua configurations
 
--- Example: Set up which-key plugin
+-- Set up which-key plugin
 if pcall(require, "which-key") then
   require("which-key").setup {
     -- Your which-key configuration here
@@ -16,3 +16,40 @@ end
 -- end
 
 -- You can add more Lua-based configurations for other Neovim-specific plugins here
+
+-- Setup lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Commented out to disable lazy.nvim
+--require("lazy").setup({}, opts)
+
+-- Only required if you have packer configured as `opt`
+--vim.cmd [[packadd packer.nvim]]
+
+--return require('packer').startup(function(use)
+--  -- Packer can manage itself
+--  use 'wbthomason/packer.nvim'
+
+--  use({
+--    "jackMort/ChatGPT.nvim",
+--      config = function()
+--        require("chatgpt").setup()
+--      end,
+--      requires = {
+--        "MunifTanjim/nui.nvim",
+--        "nvim-lua/plenary.nvim",
+--        "nvim-telescope/telescope.nvim"
+--      }
+--  })
+--end)
